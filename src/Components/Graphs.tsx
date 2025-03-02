3ac962abda0b19fb83fc71dbb410ab670b743e30
chataignault <leonchat@orange.fr>
2025-03-02 19:12:43 +0000

    update prettier format

--- src/Components/Graphs.tsx
+++ src/Components/Graphs.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Plot from "react-plotly.js";
 
-
-export default function Graph(props:{data:number[]}) {
-
-
+export default function Graph(props: { data: number[] }) {
   // const curves = data.IRData.map((curve: any, idx: any) => {
   //   return {
   //     x: Array(curve.length).keys(),
@@ -12,15 +9,17 @@ export default function Graph(props:{data:number[]}) {
   //     name: `Component ${idx + 1}`,
   //   };
   // });
-  const curves = [{
-    x: Array(props.data.length).keys(),
-    y: props.data,
-    name: 'Value'
-  }]
+  const curves = [
+    {
+      x: Array(props.data.length).keys(),
+      y: props.data,
+      name: "Value",
+    },
+  ];
 
   return (
     <>
-      <button onClick={()=> {}}>Plot PCA</button>
+      <button onClick={() => {}}>Plot PCA</button>
       <Plot data={curves} layout={{ title: "PCA", autosize: true }} />
     </>
   );
--- src/connexionStore.tsx
+++ src/connexionStore.tsx
@@ -1,6 +1,5 @@
-
-const connexionStore={
-    'local': 'http://127.0.0.1:8000/yf/'
-}
+const connexionStore = {
+  local: "http://127.0.0.1:8000/yf/",
+};
 
 export default connexionStore;
